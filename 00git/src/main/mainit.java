08a6dc6f5f6d51c34730fe45deb4a6271a77c7ca
Pablosky <pablosky971@gmail.com>
2022-03-01 12:17:03 +0100

    cambio de lectura por pantalla

--- 00git/src/main/mainit.java
+++ 00git/src/main/mainit.java
@@ -1,9 +1,14 @@
 package main;
 
+import java.util.Scanner;
+
 public class mainit {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo Java :D");
+		Scanner sc = new Scanner(System.in);
+		int numero1 = sc.nextInt();
+		System.out.println("Hemos introducido el número: " + numero1);
 		
 	}
 
