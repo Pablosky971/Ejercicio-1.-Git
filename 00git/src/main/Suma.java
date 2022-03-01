123ab580d7e101e0c733f734b2ae31a507fed964
Pablosky <pablosky971@gmail.com>
2022-03-01 14:02:17 +0100

    Clase suma con los dos métodos solicitados

---
+++ 00git/src/main/Suma.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Suma {
+	
+	private int suma;
+	
+	public static int sumaDosNumeros(int n1, int n2) {
+		return n1 + n2;
+	}
+	
+	public int sumaAcumulada(int n) {
+		 
+		int sumaAcumulada = suma + n;
+		return sumaAcumulada;
+		
+	}
+	
+
+}
