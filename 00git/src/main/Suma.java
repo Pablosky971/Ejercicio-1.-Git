e5be493efe2188355367cfd2eaa8d26abbe0f218
Pablosky <pablosky971@gmail.com>
2022-03-02 13:26:02 +0100

    Solucionado la clase suma y probada con éxito

--- 00git/src/main/Suma.java
+++ 00git/src/main/Suma.java
@@ -2,16 +2,21 @@ package main;
 
 public class Suma {
 	
-	private int suma;
 	
+
 	public static int sumaDosNumeros(int n1, int n2) {
 		return n1 + n2;
 	}
 	
-	public int sumaAcumulada(int n) {
-		 
-		int sumaAcumulada = suma + n;
-		return sumaAcumulada;
+	public static int sumaAcumulada(int ... numeros) {
+		 int acum = 0;
+		for(int n:numeros) {
+			
+			acum += n;
+			
+		}
+		
+		return acum;
 		
 	}
 	
--- 00git/src/main/mainit.java
+++ 00git/src/main/mainit.java
@@ -6,9 +6,24 @@ public class mainit {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo Java :D");
+		System.out.println("Introduzca el número que desee usar:");
 		Scanner sc = new Scanner(System.in);
 		int numero1 = sc.nextInt();
 		System.out.println("Hemos introducido el número: " + numero1);
+		
+		// Prueba de la suma de dos números
+		int sumaDosNumeros = Suma.sumaDosNumeros(5, 6);
+		System.out.println("Resultado de los dos números: " +  sumaDosNumeros);
+		
+		// Prueba de la suma acumulada
+		
+		int sumaAcumulada = Suma.sumaAcumulada(1,2,7,9,numero1);
+		System.out.println("Resultado de la suma acumulada: " + sumaAcumulada);
+		
+		System.out.println("Fin del programa. Espero que le haya sido de utilidad.");
+		
+		
+		
 		
 	}
 
